ed25606e290d2be68f5bc022e749072d833b6aa9
Leonardo Costa Santos <leonardo.costa.santos.si@gmail.com>
2021-12-28 23:05:54 -0300

    Estilizando - Parte 4 - Cores extras no TailwindCSS e Ícones

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import getUser from '../utils/getUser'
+import { FaLinkedin, FaGithub, FaTwitterSquare, FaWhatsapp } from 'react-icons/fa';
 
 const Index = ({ repos, user }) => {
   return (
@@ -8,11 +9,15 @@ const Index = ({ repos, user }) => {
         <div className='pt-32'>
           <h1 className='text-4xl uppercase pl-16'>Olá, Eu sou Leonardo Costa</h1>
           <h2 className='font-bold text-5xl uppercase pl-16'>Fullstack Developer</h2>
-          <div className='relative border-2 rounded px-16 pb-4 pt-10 mt-6'>
-            <h3 className='absolute top-0 -mt-5 py-2 px-6 text-white text-2xl font-bold uppercase bg-red-800'>Contato</h3>
-            <ul>
-              <li>linkedin</li>
+          <div className='relative border-purple border rounded px-16 pb-4 pt-10 mt-6'>
+            <h3 className='absolute top-0 -mt-5 py-2 px-6 text-white text-2xl font-bold uppercase bg-purple'>Contato</h3>
+            <ul className='flex flex-row'>
+              <a href='https://www.linkedin.com/in/leonardocostasantos/' className='cursor-pointer hover:text-purple' target="_blank"><li><FaLinkedin className='text-5xl mr-6' /></li></a>
+              <a href='https://github.com/leonardocs1' className='cursor-pointer hover:text-purple' target="_blank"><li><FaGithub className='text-5xl mr-6' /></li></a>
+              <a href='https://twitter.com/leonardoCode1' className='cursor-pointer hover:text-purple' target="_blank"><li><FaTwitterSquare className='text-5xl mr-6' /></li></a>
+              <a href='https://api.whatsapp.com/send?phone=5599991519688&text=Ol%C3%A1!%20Eu%20vi%20seu%20curr%C3%ADculo%20online%20e%20me%20interessei%20no%20seu%20perfil.%20' className='cursor-pointer hover:text-purple' target="_blank"><li><FaWhatsapp className='text-5xl mr-6' /></li></a>
             </ul>
+            <p className='mt-2'>Ou envie um email: <span className='font-bold'>leonardo.costa.santos.si@gmail.com</span></p>
           </div>
         </div>
         <div>
@@ -21,16 +26,16 @@ const Index = ({ repos, user }) => {
       </div>
 
       <div className='bg-white rounded-lg shadow-lg py-12 px-16'>
-        <h3 className='text-4xl text-center'>O que eu faço</h3>
+        <h3 className='text-4xl text-center text-purple'>O que eu faço</h3>
         <p className='text-2xl'>Fullstack Developer</p>
       </div>
 
       <div>
-        <h3 className='text-4xl font-bold text-center uppercase mt-10 mb-5'>My Education</h3>
+        <h3 className='text-4xl font-bold text-center uppercase mt-10 mb-5 text-purple'>My Education</h3>
         <div className='grid grid-cols-2 leading-none bg-white rounded-lg shadow-lg py-6'>
           {[1, 2].map(i => (
             <div className='border-dashed border-l  px-12'>
-              <h4 className='text-lg uppercase font-bold mb-2'>Grau de Graduação</h4>
+              <h4 className='text-lg uppercase font-bold mb-2 text-purple'>Grau de Graduação</h4>
               <p className='text-2xl uppercase'>Sistemas de Informação<br />
                 <span className='text-lg normal-case font-bold'> Faculdade de Balsas - Unibalsas</span></p>
             </div>))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   theme: {
     fontFamily: {
       sans: ['"Titillium Web"']
+    },
+    extend: {
+      colors: {
+        purple: '#632C89'
+      }
     }
   }
 }
